56a47531525f34d6e400ba50f8496fa9a7208813
CrixaDev <102063279+CrixaDev@users.noreply.github.com>
2022-03-21 21:00:56 +0300

    Create crìxa.js

---
+++ crìxa.js
@@ -0,0 +1,60 @@
+const crixa = require("aoi.js")
+
+const bot = new crixa.Bot({
+
+token: process.env.TOKEN, 
+
+prefix: "!", 
+
+intents: "all" 
+
+}) 
+
+const loader = new crixa.LoadCommands(bot)
+
+loader.load(bot.cmd,"./komutlar/")
+
+//callbackleri buraya yazabilirsiniz
+
+bot.onJoin()
+
+bot.onLeave()
+
+bot.onMessage()
+
+//bot status (çoğaltılabilir)
+
+bot.status({
+
+text:"Crìxa v5 Altyapı",
+
+type:"PLAYING",
+
+status:"dnd",
+
+time: 12
+
+})
+
+//değişkenler
+
+bot.variables({
+
+örnek:"değer",
+
+})
+
+//komutlar
+
+bot.command({
+
+name:"ping",
+
+code:`
+
+\`$botPingms\` pingim var!
+
+`
+
+})
+
