0130f78f10858b58a03052d253a8a7a2135d8854
CrixaDev <102063279+CrixaDev@users.noreply.github.com>
2022-03-21 21:03:20 +0300

    Create eval.js

---
+++ komutlar/eval.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  name:"eval",
+
+  aliases:"test",
+
+  description:"Kodları deneyebilirsiniz?",
+
+  code: `$eval[$message]
+
+$onlyForIDs[$botOwnerID;Tanıyamadım sen kimsin?]
+
+`
+
+} //örnek komut
+
